0c5bb2468a94311991b8e7b317c736f4f9957386
iiiiiii1 <25471406+iiiiiii1@users.noreply.github.com>
2018-11-19 17:18:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,6 +10,8 @@ fi
 redis-server &
 mongod -auth --bind_ip 127.0.0.1 --port 27017 --dbpath /data/db --fork --logpath /data/log/mongodb.log &
 wait $!
+aria2c --conf-path=/data/aria2/aria2.conf
+wait $!
 
 cd /root/PyOne
 gunicorn -k eventlet -b 0.0.0.0:34567 run:app &
